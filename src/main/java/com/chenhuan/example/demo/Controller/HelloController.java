cd997293bcb74aaa81feb51926ae0c7d00d6456c
chenhuan <hnu_sky@163.com>
2020-05-26 17:58:58 +0800

    init repo

---
+++ src/main/java/com/chenhuan/example/demo/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.chenhuan.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/Hello")
+    public  String Hello(@RequestParam(name="name",required = false,defaultValue = "World")String name, Model model)
+    {
+        model.addAttribute("name",name);
+        return "Hello";
+    }
+}
